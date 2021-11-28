611eb420fe4eef2b6fe58f04da0cbdf36403df14
IslandBoyDev25 <chezling.stoddard@gmail.com>
2021-11-28 18:29:22 -0400

    up

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 
 fn main() {
-    println!("Hello, world!");
+    let listener: TcpListener: TcpListener = 
+            TcpListener::bind(addr: "127.0.0.1:7878").unwrap();
 }
