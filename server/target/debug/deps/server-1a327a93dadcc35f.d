1bbd7bcd3dc9321e6b8e418ea4a791faac329aac
IslandBoyDev25 <chezling.stoddard@gmail.com>
2021-11-28 18:24:38 -0400

    sever changes

---
+++ server/target/debug/deps/server-1a327a93dadcc35f.d
@@ -0,0 +1,5 @@
+/Users/zionora/Dropbox (Sunleaf Construc)/Mac/Documents/Dev_Projects/Web_Server_Rust/Web_Server/server/target/debug/deps/server-1a327a93dadcc35f.rmeta: src/main.rs
+
+/Users/zionora/Dropbox (Sunleaf Construc)/Mac/Documents/Dev_Projects/Web_Server_Rust/Web_Server/server/target/debug/deps/server-1a327a93dadcc35f.d: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/deps/server-e6108e605f9e3128.d
@@ -0,0 +1,5 @@
+/Users/zionora/Dropbox (Sunleaf Construc)/Mac/Documents/Dev_Projects/Web_Server_Rust/Web_Server/server/target/debug/deps/server-e6108e605f9e3128.rmeta: src/main.rs
+
+/Users/zionora/Dropbox (Sunleaf Construc)/Mac/Documents/Dev_Projects/Web_Server_Rust/Web_Server/server/target/debug/deps/server-e6108e605f9e3128.d: src/main.rs
+
+src/main.rs:
