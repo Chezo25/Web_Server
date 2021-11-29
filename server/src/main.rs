6bfea3c104ca64a88faa66af117e76c1cdfa9873
IslandBoyDev25 <chezling.stoddard@gmail.com>
2021-11-28 20:40:20 -0400

    runing

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let listener: TcpListener = 
@@ -14,10 +15,39 @@ fn main() {
 }
 
 fn handle_connection(mut _stream: TcpStream){
-    let mut buffer:[u8; 1024] = [0; 1024];
+    let mut buffer:[u8; 1024] = [0; 1024]; // this is the limit setter
 
     _stream.read(&mut buffer).unwrap();
-    println!("Requst: {}",
-             String::from_utf8_lossy(&buffer[..])
-);
+
+    let _get = b"GET / HTTP/1.1\r\n"; // this is the bit array string
+
+    let (status_line, filename) = 
+        if buffer.starts_with(_get) {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else {
+        ("HTTP/1.1 404 NOT FOUND","404.html")
+        };
+
+    
+
+    let contents: String = fs::read_to_string(filename).unwrap();
+
+        let _response =  format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+
+        _stream.write(_response.as_bytes()).unwrap();
+        _stream.flush().unwrap(); 
+    
+
 }
+
+//HTTP Status-Code
+
+//headers Crlf
+
+//message-body
+
